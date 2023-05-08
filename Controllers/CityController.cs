e9a6ded9b35d68aaa073ca79e59719b278cd37c4
Omar <omar.mateen@soti.net>
2023-05-08 17:32:50 -0400

    adding web api

---
+++ Controllers/CityController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CityController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "Atlanta", "New York" };
+        }
+
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "Atlanta";
+        }
+    }
+}
